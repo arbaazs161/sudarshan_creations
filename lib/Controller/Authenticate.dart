0251e7115bda85c8c4cc72a3ccc5b5baf9d4c39a
Arbaaz <arbaazs161@gmail.com>
2024-09-06 19:55:36 +0530

    Initial Commit

--- lib/Controller/Authenticate.dart
+++ lib/Controller/Authenticate.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:get/state_manager.dart';
 import 'package:sudarshan_creations/Model/UserModel.dart';
 
